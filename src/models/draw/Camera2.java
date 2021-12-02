6bed8859bf2c58490065b03c43168e996e001825
Rostislav <kluskape@gmail.com>
2021-12-02 22:19:55 +0300

    Camera rewritten

--- src/models/draw/Camera2.java
+++
@@ -1,9 +0,0 @@
-package models.draw;
-
-import Jama.Matrix;
-
-public class Camera2 {
-    public final Matrix position = new Matrix(new double[][]{{0, 0, 3}});
-    public final Matrix target = new Matrix(new double[][]{{0, 0, 0}});
-    public final Matrix direction = position.minus(target);
-}
